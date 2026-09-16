3172eb0ef79d4891353c532d9f10946eb3839e0b
Chetan <chetanghunavat12800@gmail.com>
2026-09-16 12:19:27 +0530

    Updated code

--- image_mat.py
+++ image_mat.py
@@ -2,8 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt 
 from PIL import Image
 img=Image.open("girl.jpg")
-matrix=np.array(img)
-new_image=Image.fromarray(matrix)
-plt.imshow(new_image)
-plt.axis("off")
-plt.show()
+mat_data=np.array(img)
+np.save("mat_rix",mat_data)
+# mat_load=np.load('mat_rix.npy')
+# plt.figure(figsize=(10,5))
+# plt.subplot(121)
+# plt.imshow(mat_load)
+# plt.title("girl image chai")
+# plt.grid(False)
